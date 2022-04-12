f72a80f5111f752008ef2ccbad211e88685ca7ed
Daniel Armbruster <danielgundam00@gmail.com>
2022-04-12 09:28:40 -0600

    commit tree 54b878f84441b2481c3d642286631af72f438742

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,14 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :dev 
+    belongs_to :company 
 
+    def print_details
+        "#{self.dev.name} owns a #{self.item_name} from #{self.company.name}"
+    end 
+
+    def self.the_new_thing
+        self.all.map do |instance|
+            puts "#{instance} hi"
+        end
+    end
 end
